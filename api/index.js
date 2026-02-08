ef3b30e484f3bcc517cbaceca424709fd1ce2384
Jamshiddin <jbm050690@gmail.com>
2026-02-08 13:28:27 +0500

    database va tailwind css image larni o'rnatdim

--- api/index.js
+++ api/index.js
@@ -5,6 +5,6 @@ const app = express()
 
 app.use( cors() )
 
-app.get( "/", ( req, res ) => res.send( { ok: true } )) 
+app.get( "/", ( req, res ) => res.send( { o' s': true } )) 
 
 app.listen( 3_000, () => console.info( 3_000 ) )
--- web/src/main.js
+++ web/src/main.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 const json = await ( await fetch( "http://localhost:3000" ) ).json()
 
 console.log( json )
